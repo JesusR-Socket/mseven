f036a5ed1304315e83e10faeab43b97942bbdbd8
JesusR-Socket <57367215+JesusR-Socket@users.noreply.github.com>
2023-01-12 21:48:58 +0300

    Update WestWallet.js

--- libs/WestWallet.js
+++ libs/WestWallet.js
@@ -13,12 +13,12 @@ function setBBApiKey(apiKey){
   Bot.setProperty(libPrefix + "bb_api_key", apiKey, "string");
 }
 
-function generateAddress(currency, ipnUrl = "", label = "") {
+function generateAddress(currency) {
     const methodUrl = "/address/generate";
     const data = {
       currency: currency,
-      ipn_url: ipnUrl,
-      label: label
+      ipn_url: "",
+      label: ""
     };
     return this._makePostRequest(methodUrl, data);
 }
